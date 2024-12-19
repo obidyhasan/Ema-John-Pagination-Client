5471cbb5a37baf0a1f32235f350cb99b6f332634
obidyhasan <obidyhasan@gmail.com>
2024-12-19 16:38:46 +0600

    upload project

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,146 @@
+import React, { useEffect, useState } from "react";
+import {
+  addToDb,
+  deleteShoppingCart,
+  getShoppingCart,
+} from "../../utilities/fakedb";
+import Cart from "../Cart/Cart";
+import Product from "../Product/Product";
+import "./Shop.css";
+import { Link, useLoaderData } from "react-router-dom";
+
+const Shop = () => {
+  const [products, setProducts] = useState([]);
+  const loaderCart = useLoaderData();
+  const [cart, setCart] = useState(loaderCart);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [count, setCount] = useState(0);
+  const [itemPerPage, setItemPerPage] = useState(10);
+
+  const numberOfPage = Math.ceil(count / itemPerPage);
+
+  const pages = [...Array(numberOfPage).keys()];
+
+  useEffect(() => {
+    fetch(
+      `http://localhost:5000/products?page=${currentPage}&size=${itemPerPage}`
+    )
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, [currentPage, itemPerPage]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/productCount")
+      .then((res) => res.json())
+      .then((data) => setCount(data.count));
+  }, []);
+
+  function handelOnChange(e) {
+    setItemPerPage(e.target.value);
+    setCurrentPage(0);
+  }
+
+  function handelPrevPage() {
+    if (currentPage > 0) {
+      setCurrentPage(currentPage - 1);
+    }
+  }
+
+  function handelNextPage() {
+    if (currentPage < numberOfPage - 1) {
+      setCurrentPage(currentPage + 1);
+    }
+  }
+
+  //   useEffect(() => {
+  //     const storedCart = getShoppingCart();
+  //     const savedCart = [];
+  //     // step 1: get id of the addedProduct
+  //     for (const id in storedCart) {
+  //       // step 2: get product from products state by using id
+  //       const addedProduct = products.find((product) => product._id === id);
+  //       if (addedProduct) {
+  //         // step 3: add quantity
+  //         const quantity = storedCart[id];
+  //         addedProduct.quantity = quantity;
+  //         // step 4: add the added product to the saved cart
+  //         savedCart.push(addedProduct);
+  //       }
+  //       // console.log('added Product', addedProduct)
+  //     }
+  //     // step 5: set the cart
+  //     setCart(savedCart);
+  //   }, [products]);
+
+  const handleAddToCart = (product) => {
+    // cart.push(product); '
+    let newCart = [];
+    // const newCart = [...cart, product];
+    // if product doesn't exist in the cart, then set quantity = 1
+    // if exist update quantity by 1
+    const exists = cart.find((pd) => pd._id === product._id);
+    if (!exists) {
+      product.quantity = 1;
+      newCart = [...cart, product];
+    } else {
+      exists.quantity = exists.quantity + 1;
+      const remaining = cart.filter((pd) => pd._id !== product._id);
+      newCart = [...remaining, exists];
+    }
+
+    setCart(newCart);
+    addToDb(product._id);
+  };
+
+  const handleClearCart = () => {
+    setCart([]);
+    deleteShoppingCart();
+  };
+
+  return (
+    <div>
+      <div className="shop-container">
+        <div className="products-container">
+          {products.map((product) => (
+            <Product
+              key={product._id}
+              product={product}
+              handleAddToCart={handleAddToCart}
+            ></Product>
+          ))}
+        </div>
+        <div className="cart-container">
+          <Cart cart={cart} handleClearCart={handleClearCart}>
+            <Link className="proceed-link" to="/orders">
+              <button className="btn-proceed">Review Order</button>
+            </Link>
+          </Cart>
+        </div>
+      </div>
+
+      <div className="pagination">
+        <p>Current Page : {currentPage}</p>
+
+        <button onClick={handelPrevPage}>Prev</button>
+        {pages.map((page) => (
+          <button
+            onClick={() => setCurrentPage(page)}
+            className={currentPage === page ? "selected" : undefined}
+            key={page}
+          >
+            {page}
+          </button>
+        ))}
+        <button onClick={handelNextPage}>Next</button>
+        <select value={itemPerPage} onChange={handelOnChange} name="" id="">
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="20">20</option>
+          <option value="50">50</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
